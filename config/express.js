079f6a88f099ec29d9217bf362316edb3cd9ffbf
Brooks Becton <broabect@ut.utm.edu>
2018-06-06 08:09:32 -0500

    Add shorten URL UI

--- config/express.js
+++ config/express.js
@@ -14,6 +14,8 @@ const routes = require('../index.route');
 const config = require('./config');
 const APIError = require('../server/helpers/APIError');
 
+const Url = require('./../server/url/url.model');
+
 const app = express();
 
 if (config.env === 'development') {
@@ -27,6 +29,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compress());
 app.use(methodOverride());
+app.use(express.static('public'));
+app.set('view engine', 'pug');
 
 // secure apps by setting various HTTP headers
 app.use(helmet());
@@ -38,17 +42,26 @@ app.use(cors());
 if (config.env === 'development') {
   expressWinston.requestWhitelist.push('body');
   expressWinston.responseWhitelist.push('body');
-  app.use(expressWinston.logger({
-    winstonInstance,
-    meta: true, // optional: log meta data about request (defaults to true)
-    msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
-    colorStatus: true // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
-  }));
+  app.use(
+    expressWinston.logger({
+      winstonInstance,
+      meta: true, // optional: log meta data about request (defaults to true)
+      msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
+      colorStatus: true // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
+    })
+  );
 }
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
+});
+
+app.get('/:hash', (req, res) => {
+  res.redirect('/');
+});
 
 // mount all routes on /api path
 app.use('/api', routes);
-
 // if error is not an instanceOf APIError, convert it.
 app.use((err, req, res, next) => {
   if (err instanceof expressValidation.ValidationError) {
@@ -71,13 +84,20 @@ app.use((req, res, next) => {
 
 // log error in winston transports except when executing test suite
 if (config.env !== 'test') {
-  app.use(expressWinston.errorLogger({
-    winstonInstance
-  }));
+  app.use(
+    expressWinston.errorLogger({
+      winstonInstance
+    })
+  );
 }
 
 // error handler, send stacktrace only during development
-app.use((err, req, res, next) => // eslint-disable-line no-unused-vars
+app.use((
+  err,
+  req,
+  res,
+  next // eslint-disable-line no-unused-vars
+) =>
   res.status(err.status).json({
     message: err.isPublic ? err.message : httpStatus[err.status],
     stack: config.env === 'development' ? err.stack : {}
---
+++ public/js/index.js
@@ -0,0 +1,51 @@
+function postData(url, data) {
+  // Default options are marked with *
+  return fetch(url, {
+    body: JSON.stringify(data), // must match 'Content-Type' header
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, same-origin, *omit
+    headers: {
+      'user-agent': 'Mozilla/4.0 MDN Example',
+      'content-type': 'application/json'
+    },
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, cors, *same-origin
+    redirect: 'follow', // manual, *follow, error
+    referrer: 'no-referrer' // *client, no-referrer
+  }).then(response => response.json()); // parses response to JSON
+}
+
+async function shortenUrl(url) {
+  return new Promise(async (resolve, reject) => {
+    if (isUrl(url)) {
+      const result = await postData('/api/url', { url });
+      const abc = 'abcdefghijklmnopqrstuvwxyz1234567890'.split('');
+      const shortUrl = Array.from(result.hashId.toString())
+        .map(n => abc[n])
+        .join();
+      resolve(shortUrl);
+    } else {
+      reject('Not Valid URL');
+    }
+  });
+}
+
+async function main() {
+  const targetUrl = document.querySelector('.targetUrl').value;
+  let result;
+  try {
+    result = await shortenUrl(targetUrl);
+  } catch (error) {
+    result = 'Not a Valid URL';
+  }
+  document.querySelector('.resultURL').innerHTML = result;
+}
+
+function isUrl(str) {
+  regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  if (regexp.test(str)) {
+    return true;
+  } else {
+    return false;
+  }
+}
